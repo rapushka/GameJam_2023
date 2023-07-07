8a2cb90c0461f8a5ab60381ce820a185da986c00
rapushka <mikalajsiatkouski@gmail.com>
2023-07-07 21:58:53 +0300

    tryna implement pig's movement (not working)

--- src/GameJam_2023/Assets/Code/PigMovement.cs
+++ src/GameJam_2023/Assets/Code/PigMovement.cs
@@ -1,6 +1,44 @@
+using System;
+using System.Reflection;
 using UnityEngine;
 
 namespace Code
 {
-	public class PigMovement : MonoBehaviour { }
+	public class PigMovement : MonoBehaviour
+	{
+		[SerializeField] private CharacterController _characterController;
+		[SerializeField] private float _movementSpeed;
+
+		private Vector2 _movingDirection;
+
+		private static float VerticalAxis => Input.GetAxis("Vertical");
+
+		private static float HorizontalAxis => Input.GetAxis("Horizontal");
+
+		private bool IsMoving => _movingDirection.magnitude > 0f;
+
+		private float ScaledSpeed => _movementSpeed * Time.fixedDeltaTime;
+
+		private void Update()
+		{
+			_movingDirection = new Vector2(HorizontalAxis, VerticalAxis);
+		}
+
+		private void FixedUpdate()
+		{
+			if (IsMoving)
+			{
+				Rotate();
+				Move();
+			}
+		}
+
+		private void Rotate() => transform.rotation = Quaternion.LookRotation(_movingDirection.ToTopDown());
+
+		private void Move()
+		{
+			var movement = _movingDirection * ScaledSpeed;
+			_characterController.Move(movement.ToTopDown());
+		}
+	}
 }
