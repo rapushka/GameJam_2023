5ad496060ed0e7c10151dff8ffd3bd79caa08ea3
rapushka <mikalajsiatkouski@gmail.com>
2023-07-07 21:28:46 +0300

    set camera as child of pig

--- src/GameJam_2023/Assets/Code/GameBootstrap.cs
+++ src/GameJam_2023/Assets/Code/GameBootstrap.cs
@@ -5,12 +5,14 @@ namespace Code
 	public class GameBootstrap : MonoBehaviour
 	{
 		[SerializeField] private Transform _pigSpawnPoint;
+		[SerializeField] private Camera _camera;
 
 		private void Start()
 		{
-			var pigPrefab = Resources.Load("Pig/Pig");
+			var pigPrefab = Resources.Load<GameObject>("Pig/Pig");
 
 			var pig = Instantiate(pigPrefab, _pigSpawnPoint.position, Quaternion.identity);
+			_camera.transform.SetParent(pig.transform);
 		}
 	}
 }
