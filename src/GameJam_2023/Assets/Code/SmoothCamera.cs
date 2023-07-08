85477451aa860178aeef509607bb985a64fccc0f
rapushka <mikalajsiatkouski@gmail.com>
2023-07-08 04:31:50 +0300

    lerp

--- src/GameJam_2023/Assets/Code/SmoothCamera.cs
+++ src/GameJam_2023/Assets/Code/SmoothCamera.cs
@@ -4,36 +4,24 @@ namespace Code
 {
 	public class SmoothCamera : MonoBehaviour
 	{
-		[SerializeField] private float _cameraSpeed = 5f;
-		[SerializeField] private Size _levelSizes;
+		[SerializeField] private float _cameraSpeed;
 		[SerializeField] private Vector3 _offset;
 
 		private Transform _target;
 
-		private Vector3 TargetPosition { get => _target.position; set => _target.position = value; }
+		private float ScaledSpeed => _cameraSpeed * Time.deltaTime;
+
+		private Vector3 TargetPosition => _target.position + _offset;
 
 		private Vector3 CameraPosition { get => transform.position; set => transform.position = value; }
 
 		public void Construct(Transform target) => _target = target;
 
-		private void FixedUpdate()
-		{
-			MoveToTarget();
-			LimitInBounces();
-		}
-
-		private void LimitInBounces()
-		{
-			CameraPosition.Set
-			(
-				x: CameraPosition.x.Clamp(max: _levelSizes.Width),
-				z: CameraPosition.y.Clamp(max: _levelSizes.Height)
-			);
-		}
+		private void FixedUpdate() => MoveToTarget();
 
 		private void MoveToTarget()
 		{
-			CameraPosition = Vector3.MoveTowards(CameraPosition, TargetPosition + _offset, _cameraSpeed * Time.deltaTime);
+			CameraPosition = Vector3.Lerp(CameraPosition, TargetPosition, ScaledSpeed);
 		}
 	}
 }
