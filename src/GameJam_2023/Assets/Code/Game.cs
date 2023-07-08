8b650ef995221a1feeec913f9a4587d02f8aa404
rapushka <mikalajsiatkouski@gmail.com>
2023-07-08 03:48:18 +0300

    tryna implement smooth camera

--- src/GameJam_2023/Assets/Code/Game.cs
+++ src/GameJam_2023/Assets/Code/Game.cs
@@ -5,14 +5,14 @@ namespace Code
 	public class Game : MonoBehaviour
 	{
 		[SerializeField] private Transform _pigSpawnPoint;
-		[SerializeField] private Camera _camera;
+		[SerializeField] private SmoothCamera _camera;
 
 		private void Start()
 		{
 			var pigPrefab = Resources.Load<GameObject>("Pig/Pig");
 
 			var pig = Instantiate(pigPrefab, _pigSpawnPoint.position, Quaternion.identity);
-			_camera.transform.SetParent(pig.transform);
+			_camera.Construct(pig.transform);
 		}
 	}
 }
